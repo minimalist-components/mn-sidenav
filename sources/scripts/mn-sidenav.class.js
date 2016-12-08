55fc9e89f9098ec58d4cbc3468ab729dd2310a6e
Darlan Mendonça <darlanmendonca@gmail.com>
2016-12-08 17:50:41 -0200

    add styles and class to component

--- sources/scripts/mn-sidenav.class.js
+++ sources/scripts/mn-sidenav.class.js
@@ -1,106 +1,51 @@
-class MnDialog extends HTMLElement {
+class MnSidenav extends HTMLElement {
   constructor(self) {
     self = super(self)
-    this.setCard()
     this.setOpenEvents()
     this.setCloseEvents()
     return self
   }
 
-  setCard() {
-    const card = document.createElement('div')
-
-    card.classList.add('mn-card')
-
-    Array
-      .from(this.classList)
-      .filter(cardClass)
-      // assign classes to card
-      .forEach(cssClass => {
-        this.classList.remove(cssClass)
-        card.classList.add(cssClass)
-      })
-
-    Array
-      .from(this.children)
-      .forEach(putInCard)
-
-    function putInCard(element) {
-      card.appendChild(element)
-    }
-
-    this.insertBefore(card, this.firstChild)
-
-    function cardClass(cssClass) {
-      const validClasses = [
-        'padding-header',
-        'padding'
-      ]
-
-      const isValidClass = validClasses.indexOf(cssClass) > -1
-
-      return isValidClass
-    }
-  }
-
   setOpenEvents() {
-    const id = this.id
-    const buttons = document.querySelectorAll(`button[data-open-dialog="${id}"]`)
-    let openDialog = this.open
-
+    const buttons = document.querySelectorAll(`button[sidenav="${this.id}"]`)
+    const open = this.open
     Array
       .from(buttons)
-      .forEach(setEventListener)
-
-    function setEventListener(button) {
-      button.addEventListener('click', openDialog)
-    }
+      .forEach(button => button.addEventListener('click', open))
   }
 
   setCloseEvents() {
-    const buttons = document.querySelectorAll('[data-close-dialog]')
     const close = this.close
-
-    Array
-      .from(buttons)
-      .forEach(button => button.addEventListener('click', close))
-
     document.body.addEventListener('click', close)
     document.addEventListener('keyup', () => {
       const esc = event.keyCode === 27
-      let isOpened = document.body.classList.contains('mn-dialog-opened')
+      let isOpened = document.body.classList.contains('mn-sidenav-visible')
       if (esc && isOpened) {
-        const dialog = document.querySelector('mn-dialog.opened')
-        document.body.classList.remove('mn-dialog-opened')
-        dialog.classList.remove('opened')
+        const sidenav = document.querySelector('mn-sidenav.visible')
+        document.body.classList.remove('mn-sidenav-visible')
+        sidenav.classList.remove('visible')
       }
     })
   }
 
-  open() {
-    const id = event.target.getAttribute('data-open-dialog')
-    let dialog = document.querySelector(`mn-dialog#${id}`)
-
-    document.body.classList.add('mn-dialog-opened')
-
-    const previousDialog = document.querySelector('mn-dialog.opened')
-    if (previousDialog) {
-      previousDialog.classList.remove('opened')
-    }
-    dialog.classList.add('opened')
+  open(event) {
+    const id = event.target.getAttribute('sidenav')
+    const sidenav = document.querySelector(`mn-sidenav#${id}`)
+    sidenav.classList.add('visible')
+    document.body.classList.add('mn-sidenav-visible')
   }
 
   close(event) {
     event.stopPropagation()
-    const clickButtonClose = event.target.getAttribute('data-close-dialog')
-    const clickOutside = event.target.tagName === 'MN-DIALOG'
+    const clickButtonClose = event.target.getAttribute('data-close-sidenav')
+    const clickOutside = event.target.tagName === 'BODY'
+    const sidenav = document.querySelector('mn-sidenav.visible')
 
-    if (clickButtonClose || clickOutside) {
-      const dialog = document.querySelector('mn-dialog.opened')
-      document.body.classList.remove('mn-dialog-opened')
-      dialog.classList.remove('opened')
+    if ((clickButtonClose || clickOutside) && sidenav) {
+      document.body.classList.remove('mn-sidenav-visible')
+      sidenav.classList.remove('visible')
     }
   }
 }
 
-customElements.define('mn-dialog', MnDialog)
+customElements.define('mn-sidenav', MnSidenav)
